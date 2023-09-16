88f6dd84b7b7f617a14024ce7829e3bfeddb28b0
Leonardo Lujan <LeonardoLujan72@gmail.com>
2023-09-16 12:17:39 -0400

    took away link

--- src/app/courses/calculus1/problems/page.tsx
+++ src/app/courses/calculus1/problems/page.tsx
@@ -1,11 +1,9 @@
 
-"use client"
-import Link from 'next/link'
 
 
 export default function Problems() {
     return(
-        <div className="text-black">
+        <div className="justify-center flex text-black text-4xl py-8">
             These are the calc 1 problems
             
 
