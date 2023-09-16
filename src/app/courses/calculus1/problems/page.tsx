423400c21083f0c1b5249f4753d064df0eb4e347
NilufarMajidova <124289728+NilufarMajidova@users.noreply.github.com>
2023-09-16 19:56:20 -0400

    exp1

--- src/app/courses/calculus1/problems/page.tsx
+++ src/app/courses/calculus1/problems/page.tsx
@@ -18,7 +18,7 @@ export default function Problems({amount}: AmtTime) {
                   
                 </div>
                 <div className="justify-center flex text-3xl mb-4">
-                    1. f(t)=(4t^2-t)(t^3-8t^2+12) 
+                    1. 	f(t)=(<sup class = "exponent">4t</sup>^2-t)(t^3-8t^2+12) 
                 </div>
 
                 <div className="justify-center flex text-3xl mb-4"> 
