e3f4c9f13824770288612f5d751d773d0bc1125e
Leonardo Lujan <LeonardoLujan72@gmail.com>
2023-09-16 12:18:47 -0400

    Changed it back to PomStudy

--- src/app/banner.tsx
+++ src/app/banner.tsx
@@ -4,7 +4,7 @@ export default function Banner() {
     return(
         <div className="flex justify-center bg-red-500 h-36">
             <Logo/>
-            <p className="text-4xl font-mono py-12 px-3"> PoStudy </p>
+            <p className="text-4xl font-mono py-12 px-3"> PomStudy </p>
         </div>
     );
 }
