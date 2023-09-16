c7bacb6b9c1169d9ebdb9dca7592d57a63ebb137
Leonardo Lujan <LeonardoLujan72@gmail.com>
2023-09-16 17:05:28 -0400

    Deleted my contributions to calculus1

--- src/app/courses/calculus1/problems/page.tsx
+++ src/app/courses/calculus1/problems/page.tsx
@@ -7,9 +7,8 @@ import App from "@/app/components/studytime/App"
 
 export default function Problems({amount}: AmtTime) {
     return(
-<<<<<<< HEAD
         <div className="justify-center flex text-black text-4xl py-8">
-            These are the calc 1 problems:
+            <p>These are the calc 1 problems:
                 1. f(t)=(4t^2-t)(t^3-8t^2+12) 
                 2. g(w)=(3w+w^4)/(2w^2+1)
             
@@ -17,17 +16,11 @@ export default function Problems({amount}: AmtTime) {
                 1. f'(t)=(8t-1)(t^3-8t^2+12)+(4t^2-t)(3t^2-16t)=20t^4-132t^3+24t^2+96t-12
                 2. g'(w)=((3+4w^3)(2w^2+1)-(3w+w^4)(4w))/(2w^2+1)^2=(4w^5+4w^3-6w^2+3)/(2w^2+1)^2
 
-
+            </p>
             <Time/>
-            
-=======
-        <div className=" text-black text-4xl py-8">
-            <div className="justify-center flex">
-            These are the calc 1 problems
-            
-            <TimeComponent amount={15}/>
             
 
         </div>
+    
     )
 }
