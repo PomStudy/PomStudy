76621bab8ba5a0055f322b1fc1c93a2c5fd4a729
NilufarMajidova <124289728+NilufarMajidova@users.noreply.github.com>
2023-09-16 16:28:06 -0400

    Update page.tsx

--- src/app/courses/calculus1/problems/page.tsx
+++ src/app/courses/calculus1/problems/page.tsx
@@ -4,8 +4,11 @@
 export default function Problems() {
     return(
         <div className="justify-center flex text-black text-4xl py-8">
-            These are the calc 1 problems
-            
+            <p>
+                These are the calc 1 problems:
+                1. f(t)=(4t^2-t)(t^3-8t^2+12)
+                2. g(w)=(3w+w^4)/(2w^2+1)
+            </p>
 
         </div>
     )
