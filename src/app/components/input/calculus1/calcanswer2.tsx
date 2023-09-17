f9132f637286b200c8de4f61cbd6e729c4e2ea37
Leonardo Lujan <LeonardoLujan72@gmail.com>
2023-09-17 03:18:28 -0400

    updated some files

--- src/app/components/input/calculus1/calcanswer2.tsx
+++ src/app/components/input/calculus1/calcanswer2.tsx
@@ -3,10 +3,17 @@ import {useState} from 'react';
 
 
 export default function CalcAnswer2() {
-    const [attempts, setAttempts] = useState<number>(0);
+    const [attempts, setAttempts] = useState<number>(3);
 
     function handleClick() {
-        setAttempts(attempts + 1);
+        {if (attempts > 0) {
+
+            setAttempts(attempts - 1)
+        } else {
+            setAttempts(0);
+        }}
+
+            
     }
 
     function increment() {
@@ -17,15 +24,15 @@ export default function CalcAnswer2() {
     <div className="w-full max-w-x py-4" >
         <div className="justify-center flex">
             <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={increment} autoComplete="off">
-                <div className=" mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" >
-                        You have {3 - attempts} attempts left
+                <div className="mb-4">
+                    <label className="flex justify-center block text-gray-700 text-sm font-bold mb-2" >
+                        You have {attempts} attempts left
                     </label>
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder=""/>
                 </div>
             
             <div className="flex justify-center">
-                <button className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={handleClick} >
+                <button className="bg-red-500 hover:bg-red-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={handleClick} >
                     Submit
                 </button>
                 
@@ -33,7 +40,7 @@ export default function CalcAnswer2() {
             </form>
         </div>
 
-        {attempts > 2 && 
+        {attempts == 0 && 
             <div className="justify-center flex">
                 <div className="justify-center flex">
                 2. dg/dw=((3+4w^3)(2w^2+1)-(3w+w^4)(4w))/(2w^2+1)^2
--- src/app/components/input/physics2/phyanswer3.tsx
+++ src/app/components/input/physics2/phyanswer3.tsx
@@ -3,15 +3,19 @@ import {useState} from 'react';
 
 
 export default function PhyAnswer3() {
-    const [attempts, setAttempts] = useState<number>(0);
+    const [attempts, setAttempts] = useState<number>(3);
 
     function handleClick() {
-        setAttempts(attempts + 1);
-    }
-
-    function increment() {
-        setAttempts(attempts + 1);
-    }
+        {if (attempts > 0) {
+    
+            setAttempts(attempts - 1)
+        } else {
+            setAttempts(0);
+        }}}
+    
+        function increment() {
+            setAttempts(attempts + 1);
+        }
     
     return(
     <div className="w-full max-w-x py-4" >
@@ -19,13 +23,13 @@ export default function PhyAnswer3() {
             <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={increment} autoComplete="off">
                 <div className=" mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" >
-                        You have {3 - attempts} attempts left
+                        You have {attempts} attempts left
                     </label>
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder=""/>
                 </div>
             
             <div className="flex justify-center">
-                <button className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={handleClick} >
+                <button className="bg-red-500 hover:bg-red-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={handleClick} >
                     Submit
                 </button>
                 
@@ -33,7 +37,7 @@ export default function PhyAnswer3() {
             </form>
         </div>
 
-        {attempts > 2 && 
+        {attempts == 0 && 
             <div className="justify-center flex text-4xl">
                 <div className="border-4 border-red-500 rounded-full p-3 justify-center">
                 a) 3.7 x 10&#8311; m/s 
