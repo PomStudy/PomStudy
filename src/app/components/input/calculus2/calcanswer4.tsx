25c32e852038df263cf1b266d49b8e754782ad85
Leonardo Lujan <LeonardoLujan72@gmail.com>
2023-09-17 00:59:05 -0400

    autocomplete-> autoComplete

--- src/app/components/input/calculus2/calcanswer4.tsx
+++ src/app/components/input/calculus2/calcanswer4.tsx
@@ -17,7 +17,7 @@ export default function CalcAnswer4() {
     return(
     <div className="w-full max-w-x py-4" >
         <div className="justify-center flex">
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={increment} autocomplete="off">
+            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={increment} autoComplete="off">
                 <div className=" mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" >
                         You have {3 - attempts} attempts left
