a4aeee096484d527218d8fb19b3f41528b143e69
NilufarMajidova <124289728+NilufarMajidova@users.noreply.github.com>
2023-09-16 20:30:31 -0400

    div3

--- src/app/courses/calculus1/problems/page.tsx
+++ src/app/courses/calculus1/problems/page.tsx
@@ -30,7 +30,7 @@ export default function Problems({amount}: AmtTime) {
                 </div>
                 <div>
                     <p>
-                        1. df/dt=(8t-1)(t&#179;-8t &#178; +12)+(4t&#178;-t)(3t&#178;-16t)=20t&#8308;-132t&#179;+24t&#178;+96t-12
+                        1. <mfrac> <mn>df</mn> <mi>dt </mi> </mfrac>=(8t-1)(t&#179;-8t &#178; +12)+(4t&#178;-t)(3t&#178;-16t)=20t&#8308;-132t&#179;+24t&#178;+96t-12
                     </p>
                 </div>
                 <div>
