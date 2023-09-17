8d3873f197a3b6dc14c0122a6ec77029fe3be143
NilufarMajidova <124289728+NilufarMajidova@users.noreply.github.com>
2023-09-16 20:02:32 -0400

    exp4

--- src/app/courses/calculus1/problems/page.tsx
+++ src/app/courses/calculus1/problems/page.tsx
@@ -22,7 +22,7 @@ export default function Problems({amount}: AmtTime) {
                 </div>
 
                 <div className="justify-center flex text-3xl mb-4"> 
-                    2. g(w)=(3w+w^4)/(2w^2+1)
+                    2. g(w)=(3w+w<sup class = "exponent">4</sup>)/(2w<sup class = "exponent">2</sup>+1)
                 </div>
 
                 <div className="justify-center flex m-8 text-6xl font-medium">
@@ -30,11 +30,11 @@ export default function Problems({amount}: AmtTime) {
                 </div>
                 <div>
                     <p>
-                        1. df/dt=(8t-1)(t^3-8t^2+12)+(4t^2-t)(3t^2-16t)=20t^4-132t^3+24t^2+96t-12
+                        1. df/dt=(8t-1)(t<sup class = "exponent">3</sup>-8t<sup class = "exponent">2</sup>+12)+(4t<sup class = "exponent">2</sup>-t)(3t<sup class = "exponent">2</sup>-16t)=20t<sup class = "exponent">4</sup>-132t<sup class = "exponent">3</sup>+24t<sup class = "exponent">2</sup>+96t-12
                     </p>
                 </div>
                 <div>
-                    2. dg/dt=((3+4w^3)(2w^2+1)-(3w+w^4)(4w))/(2w^2+1)^2=(4w^5+4w^3-6w^2+3)/(2w^2+1)^2
+                    2. dg/dt=((3+4w<sup class = "exponent">3</sup>)(2w<sup class = "exponent">2</sup>+1)-(3w+w<sup class = "exponent">4</sup>)(4w))/(2w<sup class = "exponent">2</sup>+1)<sup class = "exponent">2</sup>=(4w<sup class = "exponent">5</sup>+4w<sup class = "exponent">3</sup>-6w<sup class = "exponent">2</sup>+3)/(2w<sup class = "exponent">2</sup>+1)<sup class = "exponent">2</sup>
                 </div>
 
             
